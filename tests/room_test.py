f975401d9c72a7a3943e6d77ef794de6ddea5b60
codeclan student <codeclanstudent@codeclans-MacBook-Pro.local>
2021-01-15 15:00:46 +0000

    set up room class + 4th test pass

---
+++ tests/room_test.py
@@ -0,0 +1,10 @@
+import unittest
+from classes.room import Room
+
+class TestRoom(unittest.TestCase):
+
+    def setUp(self):
+        self.room = Room(1)
+    
+    def test_room_has_room_number(self):
+        self.assertEqual(1, self.room.room_number)
