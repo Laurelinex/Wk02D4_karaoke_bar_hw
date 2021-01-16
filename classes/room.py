1e66ac1e6d0fe542fe86431efe7a2906d9ab11b2
codeclan student <codeclanstudent@codeclans-MacBook-Pro.local>
2021-01-16 12:40:00 +0000

    room playlist and behaviours added, 9-11 tests pass

--- classes/room.py
+++ classes/room.py
@@ -3,9 +3,16 @@ class Room:
     def __init__(self, room_number):
         self.room_number = room_number
         self.guests = []
+        self.playlist = []
     
     def check_in_guest(self, guest):
         self.guests.append(guest)
     
     def check_out_guest(self, guest):
         self.guests.remove(guest)
+    
+    def add_song_to_playlist(self, song):
+        self.playlist.append(song)
+
+    def remove_song_from_playlist(self, song):
+        self.playlist.remove(song)
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,12 +1,14 @@
 import unittest
 from classes.room import Room
 from classes.guest import Guest
+from classes.song import Song
 
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
         self.room = Room(1)
         self.guest = Guest("Carole Singer")
+        self.song = Song("You Can't Stop the Beat", "Hairspray")
     
     def test_room_has_room_number(self):
         self.assertEqual(1, self.room.room_number)
@@ -22,3 +24,15 @@ class TestRoom(unittest.TestCase):
         self.room.check_in_guest(self.guest)
         self.room.check_out_guest(self.guest)
         self.assertEqual(0, len(self.room.guests))
+    
+    def test_room_playlist_starts_empty(self):
+        self.assertEqual(0, len(self.room.playlist))
+    
+    def test_room_can_add_song_to_playlist(self):
+        self.room.add_song_to_playlist(self.song)
+        self.assertEqual(1, len(self.room.playlist))
+    
+    def test_room_can_remove_song_from_playlist(self):
+        self.room.add_song_to_playlist(self.song)
+        self.room.remove_song_from_playlist(self.song)
+        self.assertEqual(0, len(self.room.playlist))
