a6cb74a8ed80d90263e7ecb337f4b8445051e247
codeclan student <codeclanstudent@codeclans-MacBook-Pro.local>
2021-01-16 17:44:36 +0000

    entry fee added - 16th test pass

--- classes/karaoke.py
+++ classes/karaoke.py
@@ -1,4 +1,5 @@
 class Karaoke:
      
-     def __init__(self, name):
+     def __init__(self, name, entry_fee):
          self.name = name
+         self.entry_fee = entry_fee
--- tests/karaoke_test.py
+++ tests/karaoke_test.py
@@ -4,7 +4,10 @@ from classes.karaoke import Karaoke
 class TestKaraoke(unittest.TestCase):
 
     def setUp(self):
-        self.karaoke = Karaoke("The Broadway Melodies")
+        self.karaoke = Karaoke("The Broadway Melodies", 10.00)
     
     def test_karaoke_has_name(self):
         self.assertEqual("The Broadway Melodies", self.karaoke.name)
+    
+    def test_karaoke_has_entree_fee(self):
+        self.assertEqual(10.00, self.karaoke.entry_fee)
