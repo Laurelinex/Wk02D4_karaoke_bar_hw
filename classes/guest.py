3dd7e6ac152a8aeb29b0a3732488a70a6a8f02e1
codeclan student <codeclanstudent@codeclans-MacBook-Pro.local>
2021-01-17 13:02:47 +0000

    1st adv. extension complete: fav song, guest cheers and complains behaviours added

--- classes/guest.py
+++ classes/guest.py
@@ -3,6 +3,7 @@ class Guest:
     def __init__(self, name, wallet):
         self.name = name
         self.wallet = wallet
+        self.favourite_song = ""
     
     def sufficient_funds(self, price):
         return self.wallet >= price
@@ -12,3 +13,12 @@ class Guest:
             self.wallet -= entry_fee
         else: 
             return "You don't have enough cash."
+    
+    def choose_favourite_song(self, song):
+        self.favourite_song = song
+    
+    def check_playlist_for_song(self, playlist, song):
+        if song in playlist:
+            return "Whoo!"
+        else:
+            return "This place sucks..."
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,12 +1,16 @@
 import unittest
 from classes.guest import Guest
 from classes.karaoke import Karaoke
+from classes.song import Song
+from classes.room import Room
 
 class TestGuest(unittest.TestCase):
 
     def setUp(self):
         self.guest = Guest("Carole Singer", 30.00)
         self.karaoke = Karaoke("The Broadway Melodies", 10.00)
+        self.song = Song("Omigod You Guys", "Legally Blonde")
+        self.room = Room(1, 5)
     
     def test_guest_has_name(self):
         self.assertEqual("Carole Singer", self.guest.name)
@@ -30,3 +34,20 @@ class TestGuest(unittest.TestCase):
         a_not_wealthy_guy.pay_entry_fee(self.karaoke.entry_fee)
         self.assertEqual(2.00, a_not_wealthy_guy.wallet)
         self.assertEqual("You don't have enough cash.", a_not_wealthy_guy.pay_entry_fee(self.karaoke.entry_fee))
+    
+    def test_guest_can_choose_favourite_song(self):
+        self.guest.choose_favourite_song(self.song.title)
+        self.assertEqual("Omigod You Guys", self.guest.favourite_song)
+    
+    def test_guest_can_cheer_if_fav_song_in_playlist(self):
+        self.room.add_song_to_playlist(self.song)
+        self.guest.choose_favourite_song(self.song)
+        self.guest.check_playlist_for_song(self.room.playlist, self.song)
+        self.assertEqual("Whoo!", self.guest.check_playlist_for_song(self.room.playlist, self.song))
+    
+    def test_guest_complains_if_fav_song_not_in_playlist(self):
+        a_song = Song("One Day More", "Les Misérables")
+        self.room.add_song_to_playlist(self.song)
+        self.guest.choose_favourite_song(a_song)
+        self.guest.check_playlist_for_song(self.room.playlist, self.guest.favourite_song)
+        self.assertEqual("This place sucks...", self.guest.check_playlist_for_song(self.room.playlist, self.guest.favourite_song))
