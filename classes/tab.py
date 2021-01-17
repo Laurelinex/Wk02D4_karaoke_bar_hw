43a112d72b419e5a9b6ee26a049181f7aea7103b
codeclan student <codeclanstudent@codeclans-MacBook-Pro.local>
2021-01-17 15:50:29 +0000

    2nd adv. extension complete: tab keeps track of spending

--- classes/tab.py
+++ classes/tab.py
@@ -2,8 +2,23 @@ class Tab:
 
     def __init__(self):
         self.tabs = {}
-    
+        # intended: tabs = {guest: $spending}    
+        # tabs[x]: returns the spending of x
+
     def add_guest_to_tab(self, guest):
         if guest not in self.tabs.keys():
-            self.tabs[guest] = guest
-    
+            self.tabs[guest] = 0
+
+    def add_to_guest_tab(self, guest, amount):
+        if guest in self.tabs.keys():
+            self.tabs[guest] += amount
+        else: 
+            return "This guest does not have a tab open."
+
+    def check_amount_spent(self, guest):
+        if guest in self.tabs.keys():
+            return self.tabs[guest]
+        else:
+            return 0
+        
+
--- tests/tab_test.py
+++ tests/tab_test.py
@@ -14,3 +14,13 @@ class TestTab(unittest.TestCase):
     def test_tab_can_add_guest(self):
         self.tab.add_guest_to_tab(self.guest)
         self.assertEqual(1, len(self.tab.tabs))
+    
+    def test_tab_can_check_amount_spent(self):
+        self.tab.check_amount_spent(self.guest)
+        self.assertEqual(0, self.tab.check_amount_spent(self.guest))
+    
+    def test_tab_can_add_amount_to_guest_tab(self):
+        some_amount = 5.00
+        self.tab.add_guest_to_tab(self.guest)
+        self.tab.add_to_guest_tab(self.guest, some_amount)
+        self.assertEqual(5.00, self.tab.check_amount_spent(self.guest))
